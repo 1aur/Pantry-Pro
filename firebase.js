70780d2ec90aec4f91fab15e5671d8d513505707
Lauren <lauren.rodriguez22@my.stjohns.edu>
2024-08-07 21:21:01 -0400

    Update Firebase configuration, package files, and .gitignore

--- firebase.js
+++ firebase.js
@@ -1,23 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAfbH-qn1R98A-Ao215nFUvT2V4-sJbDI",
-  authDomain: "inventory-tracker-49c73.firebaseapp.com",
-  projectId: "inventory-tracker-49c73",
-  storageBucket: "inventory-tracker-49c73.appspot.com",
-  messagingSenderId: "829929968276",
-  appId: "1:829929968276:web:061929d901fea30e949a81",
-  measurementId: "G-2JDD22RWG6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
-export {firestore};
+export { firestore };
